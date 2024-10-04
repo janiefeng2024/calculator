10da793af5c2482eafd33dcd1c90b809f845bb4e
janiefeng2024 <fengjintao@gmail.com>
2024-10-04 10:27:15 -0700

    Update DivideOperation.java

--- src/main/operations/DivideOperation.java
+++ src/main/operations/DivideOperation.java
@@ -6,6 +6,9 @@ import org.springframework.stereotype.Component;
 public class DivideOperation implements OperationMethod {
     @Override
     public Number apply(Number num1, Number num2) {
+       if (num2.doubleValue() == 0) {
+            throw new ArithmeticException("Cannot divide by zero.");
+        }
         return num1.doubleValue() / num2.doubleValue();
     }
 }
