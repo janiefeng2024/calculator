2bf4fa6c2d9d2e402d5a5a6aa9ee03c093b02414
janiefeng2024 <fengjintao@gmail.com>
2024-10-04 10:26:55 -0700

    Update SubtractOperation.java

--- src/main/operations/SubtractOperation.java
+++ src/main/operations/SubtractOperation.java
@@ -5,9 +5,6 @@ import org.springframework.stereotype.Component;
 public class SubtractOperation implements OperationMethod {
     @Override
     public Number apply(Number num1, Number num2) {
-        if (num2.doubleValue() == 0) {
-            throw new ArithmeticException("Cannot divide by zero.");
-        }
         return num1.doubleValue() - num2.doubleValue();
     }
 }
